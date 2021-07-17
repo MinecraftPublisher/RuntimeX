b6881bb8cd6608c68a9eb49f796d07ac33185d0c
Martia Foroud <44898501+MinecraftPublisher@users.noreply.github.com>
2021-07-17 04:10:03 +0000

    View code comments for details.

--- runtimex.js
+++ runtimex.js
@@ -6,3 +6,22 @@
  * Licensed under the MIT license.
  * 
  */
+
+// Creates a DOM observer callback to monitor the body element and detect added script tags.
+var observeDOM = (function(){
+  var MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+
+  return function( obj, callback ){
+    if( !obj || obj.nodeType !== 1 ) return; 
+
+    if( MutationObserver ){
+      var mutationObserver = new MutationObserver(callback)
+      mutationObserver.observe( obj, { childList:true, subtree:true })
+      return mutationObserver
+    }
+    else if( window.addEventListener ){
+      obj.addEventListener('DOMNodeInserted', callback, false)
+      obj.addEventListener('DOMNodeRemoved', callback, false)
+    }
+  }
+})()
